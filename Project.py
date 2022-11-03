042e48efc5bbbb56d63db3d41c5cdf4fe78b4aea
IMFeeferz <aakhta5@uic.edu>
2022-11-03 12:36:46 -0500

    Initial Starter

---
+++ Project.py
@@ -0,0 +1,16 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+wdir = 'C:/Users/afeef/OneDrive/Documents/ECE_415/Project'
+
+while 1:  
+    if pyautogui.locateOnScreen('flower.png', confidence = 0.8) != None:
+        print("I can see it")
+        time.sleep(0.5)
+    else:
+        print("I cannot see it")
+        time.sleep(0.5)
